248b41efa4023debc233e27590b4b5abb5169b1b
uraway <masato.u0107@gmail.com>
2016-03-10 13:15:33 +0900

    change name with hubot_uraway

--- local_run.sh
+++ local_run.sh
@@ -5,4 +5,4 @@ export HUBOT_TWITTER_SECRET="LsCKlzuCfGBVxttTCUJyIiwdFMCXT7oi6ymgrvXzYSDJLSG1Zm"
 export HUBOT_TWITTER_TOKEN="707761367827152896-OpW0xunGlsqbH54UUu4Q48E3s6RgHb1"
 export HUBOT_TWITTER_TOKEN_SECRET="TiOtIJ7mRjGHzimPh8EJmZY3qeHZ1GTsUrIEEReHhsJCg"
 
-bin/hubot -a twitter -n HogeTweetBot
+bin/hubot -a twitter -n hubot_uraway
