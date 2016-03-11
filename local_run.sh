bc15d3738759a97cd05a696e23186e602d77e8e3
uraway <masato.u0107@gmail.com>
2016-03-12 00:36:35 +0900

    :fire: remove

--- local_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export HUBOT_TWITTER_KEY="yXFjTuqZGOmaSsXIkstCrON0H"
-export HUBOT_TWITTER_SECRET="LsCKlzuCfGBVxttTCUJyIiwdFMCXT7oi6ymgrvXzYSDJLSG1Zm"
-export HUBOT_TWITTER_TOKEN="707761367827152896-OpW0xunGlsqbH54UUu4Q48E3s6RgHb1"
-export HUBOT_TWITTER_TOKEN_SECRET="TiOtIJ7mRjGHzimPh8EJmZY3qeHZ1GTsUrIEEReHhsJCg"
-export HUBOT_NAME="hubot_uraway"
-
-bin/hubot -a twitter-userstream
